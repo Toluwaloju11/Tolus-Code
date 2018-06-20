98f10ea3f421910c0e47890a15b10d08cc6f57c7
toluwaloju11 <toluwaloju11@gmail.com>
2018-06-20 20:35:17 +0100

    Fixed proj

---
+++ AbisogunMSTest/AbisogunMSTest/AssemblyInt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToluMSTestFramework
+{
+    [TestClass]
+    public class AssemblyInt
+    {
+        [AssemblyInitialize]
+        public static void BeforeAssembly(TestContext tc)
+        {
+            Console.WriteLine("This is before Assembly");
+        }
+        [AssemblyCleanup]
+        public static void AfterAssembly()
+        {
+            Console.WriteLine("This is After Assembly");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
