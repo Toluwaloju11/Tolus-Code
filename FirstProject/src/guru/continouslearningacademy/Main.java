6326d7c08ed498e29d68ce2a1af1f3dbbbef7588
toluwaloju11 <toluwaloju11@gmail.com>
2018-09-12 13:39:34 +0100

    hi

---
+++ FirstProject/src/guru/continouslearningacademy/Main.java
@@ -0,0 +1,12 @@
+package guru.continouslearningacademy;
+
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+System.out.println("Hello World from EBRD");
+int age;
+age = 24;
+System.out.println("My age is " + age);
+  }
+}
